6da8d66c0fa7c4114f9c5f3f218a1aa8767a2108
Nandun Nethmina <e19170@eng.pdn.ac.lk>
2023-06-18 18:55:46 +0530

    Update HomeFragment.java

--- app/src/main/java/com/example/lmsapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/lmsapplication/ui/home/HomeFragment.java
@@ -15,31 +15,73 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.lmsapplication.LoginAndRegster.LoginActivity;
-import com.example.lmsapplication.databinding.FragmentHomeBinding;
+//import com.example.lmsapplication.AcceptReturnsActivity;
+import com.example.lmsapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         // Initialize the ViewModel
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
-        final TextView textView = binding.textHome;
+        final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         // Customize UI based on user category (admin, staff, user)
         String userCategory = getCurrentUserCategory();
-        customizeUI(userCategory);
+        customizeUI(root, userCategory);
 
-        // Handle user details button click event for admins
-        Button userDetailsButton = binding.userDetailsButton;
+        return root;
+    }
+
+    private String getCurrentUserCategory() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            // Implement your logic to retrieve the current user's category
+            // Return the user category as a string (e.g., "admin", "staff", "user")
+            // This is just a placeholder method, replace it with your own implementation
+            return "admin";
+        } else {
+            return "user";
+        }
+    }
+
+    private void customizeUI(View root, String userCategory) {
+        LinearLayout userDetailsLayout = root.findViewById(R.id.userDetailsLayout);
+        Button userDetailsButton = root.findViewById(R.id.userDetailsButton);
+        Button staffDetailsButton = root.findViewById(R.id.staffDetailsButton);
+        Button acceptBookRequestButton = root.findViewById(R.id.acceptBookRequestButton);
+        Button acceptBookReturnsButton = root.findViewById(R.id.acceptBookReturnsButton);
+
+        // Customize UI based on user category
+        if (userCategory.equals("admin") || userCategory.equals("staff")) {
+            userDetailsLayout.setVisibility(View.VISIBLE);
+            userDetailsButton.setVisibility(View.VISIBLE);
+            staffDetailsButton.setVisibility(View.VISIBLE);
+
+            if (userCategory.equals("admin")) {
+                acceptBookRequestButton.setVisibility(View.VISIBLE);
+                acceptBookReturnsButton.setVisibility(View.VISIBLE);
+            } else {
+                acceptBookRequestButton.setVisibility(View.GONE);
+                acceptBookReturnsButton.setVisibility(View.GONE);
+            }
+        } else {
+            userDetailsLayout.setVisibility(View.GONE);
+            userDetailsButton.setVisibility(View.GONE);
+            staffDetailsButton.setVisibility(View.GONE);
+            acceptBookRequestButton.setVisibility(View.GONE);
+            acceptBookReturnsButton.setVisibility(View.GONE);
+        }
+
+        // Handle user details button click event
         userDetailsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,8 +91,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        // Handle staff details button click event for admins
-        Button staffDetailsButton = binding.staffDetailsButton;
+        // Handle staff details button click event
         staffDetailsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,36 +101,22 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        return root;
-    }
+        // Handle accept book request button click event
+        acceptBookRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open accept book request activity
+                startActivity(new Intent(getActivity(), AcceptRequestActivity.class));
+            }
+        });
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
-    private String getCurrentUserCategory() {
-        // Implement your logic to retrieve the current user's category
-        // Return the user category as a string (e.g., "admin", "staff", "user")
-        // This is just a placeholder method, replace it with your own implementation
-        return "admin";
-    }
-
-    private void customizeUI(String userCategory) {
-        LinearLayout userDetailsLayout = binding.userDetailsLayout;
-        Button userDetailsButton = binding.userDetailsButton;
-        Button staffDetailsButton = binding.staffDetailsButton;
-
-        // Customize UI based on user category
-        if (userCategory.equals("admin")) {
-            userDetailsLayout.setVisibility(View.VISIBLE);
-            userDetailsButton.setVisibility(View.VISIBLE);
-            staffDetailsButton.setVisibility(View.VISIBLE);
-        } else {
-            userDetailsLayout.setVisibility(View.GONE);
-            userDetailsButton.setVisibility(View.GONE);
-            staffDetailsButton.setVisibility(View.GONE);
-        }
+        // Handle accept book returns button click event
+        acceptBookReturnsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open accept book returns activity
+                startActivity(new Intent(getActivity(), AcceptReturnsActivity.class));
+            }
+        });
     }
 }
